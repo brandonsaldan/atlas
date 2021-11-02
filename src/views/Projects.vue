169d4ad71eae9bf1a7edbc73de7177cba48f4ba4
brandonsaldan <26472557+brandonsaldan@users.noreply.github.com>
2021-11-02 02:31:44 -0400

    feat: add animations

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,7 +3,7 @@
     <div class="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
       <div class="flex flex-col justify-between h-screen">
         <main class="mb-auto">
-          <div class="divide-y divide-gray-200 dark:divide-gray-700">
+          <div class="divide-y divide-gray-200 dark:divide-gray-700 animate__animated animate__fadeIn">
             <div class="pt-6 pb-8 space-y-2 md:space-y-5">
               <h1 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">Projects</h1>
             </div>
